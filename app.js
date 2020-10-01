5a7662ba123a7d9b5dc9bb5f4077b36ba77c4138
David Huang <huangdavid42@yahoo.com>
2020-10-01 12:59:22 -0400

    finished sequelize essentials

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+// const { db } = require('./models');
+const { db, Page, User } = require("./models");
+
+const morgan = require("morgan");
+
+const layout = require("./views/layout");
+
+const PORT = 3000;
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
+app.get("/", (req, res) => {
+  res.send(layout("hello world"));
+});
+
+const init = async () => {
+  await db.sync({ force: true });
+
+  try {
+    // console.log(Page);
+    await Page.sync();
+    await User.sync();
+  } catch (error) {
+    console.error(error);
+  }
+
+  // make sure that you have a PORT constant
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
+
+// app.listen(3000);
